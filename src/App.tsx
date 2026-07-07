44551b431c5556b1baf7239d1f89dd4c4e20988d
Surabhi Bhadkamkar <surabhibhadkamkar@gmail.com>
2026-07-07 22:09:10 +0530

    Change logo image path in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import ContactSection from '@/components/ContactSection';
 import PrismaLandingPage from './components/PrismaLandingPage';
 import { TEMPLATE_CATEGORIES } from './data/templatesData';
 import { TransparentLogo } from './components/TransparentLogo';
-const logoImg = "/src/assets/images/catalyst_kalpana_1783433883960.jpg";
+const logoImg = "/logo img.png";
 
 const BG_IMAGE_1 = "https://images.higgs.ai/?default=1&output=webp&url=https%3A%2F%2Fd8j0ntlcm91z4.cloudfront.net%2Fuser_38xzZboKViGWJOttwIXH07lWA1P%2Fhf_20260609_195923_b0ba8ace-1d1d-4f2c-9a28-1ab84b330680.png&w=1280&q=85";
 const BG_IMAGE_2 = "https://images.higgs.ai/?default=1&output=webp&url=https%3A%2F%2Fd8j0ntlcm91z4.cloudfront.net%2Fuser_38xzZboKViGWJOttwIXH07lWA1P%2Fhf_20260609_201152_bba90a12-bf12-459f-91f0-51f237dbaf3b.png&w=1280&q=85";
